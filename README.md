5f0678f330f7374b3fec191ff022ad1fa573de8f
Maël Valais <mael@vls.dev>
2021-09-16 11:26:12 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# Secret Transformer
+
+The cert-manager issuers store the X.509 keys and certificates in Secret
+resources of the form:
+
+```yaml
+kind: Secret
+data:
+  tls.crt: <certificate>
+  tls.key: <key>
+```
+
+A common request reported in the cert-manager issue
+[#843](https://github.com/jetstack/cert-manager/issues/843) is to create a PEM
+bundle containing both the key and certificate for easier use with HAProxy and
+other applications that require a unified PEM bundle.
+
+You can run the `secret-transform` controller (right now, it has to be run
+out-of-cluster since I did not write any manifest) and if you annotate your
+Secret with the following annotation:
+
+```yaml
+apiVersion: v1
+kind: Secret
+metadata:
+  annotations:
+    cert-manager.io/secret-transform: tls.pem
+data:
+  tls.crt: LS0tLS1CRUdJTiBDRVJUSUZJQ0FU...CBDRVJUSUZJQ0FURS0tLS0tCg==
+  tls.key: LS0tLS1CRUdJToCi0tLS0tRU5EIF...SBQUklWQVRFIEtFWS0tLS0tCg==
+```
+
+then a new data key will be created with the name `tls.pem` and the value
+contains the key and certificate concatenated:
+
+```yaml
+apiVersion: v1
+kind: Secret
+metadata:
+  annotations:
+    cert-manager.io/secret-transform: tls.pem
+data:
+  tls.crt: LS0tLS1CRUdJTiBDRVJUSUZJQ0FU...CBDRVJUSUZJQ0FURS0tLS0tCg==
+  tls.key: LS0tLS1CRUdJToCi0tLS0tRU5EIF...SBQUklWQVRFIEtFWS0tLS0tCg==
+  tls.pem: LS0tLS1CRUdJTiBSUXc0ZHk3NTNl...kQgQ0VSVElGSUNBVEUtLS0tLQo= # ✨
+```
+
+The updated Secret looks like this:
+
+```sh
+$ kubectl get secret example -ojsonpath='{.data.tls\.pem}' | base64 -d
+-----BEGIN RSA PRIVATE KEY-----
+MIIEowIBAAKCAQEAzmuXe0BSZqjh7V94wfTifk/5hKS/V1RjyBa4RVdFBBHNGsUb
+u+8UhhRgadS+R5ZrcErpt1YIchNuliqaZbXEW0BpWtRc3NmqDRzh
+-----END RSA PRIVATE KEY-----
+-----BEGIN CERTIFICATE-----
+MIIFXTCCBEWgAwIBAgISBP8i8Bm2p/jl6yxMoLrrJlQkMA0GCSqGSIb3DQEBCwUA
+tBpwpdCVsgQqdy69SIU4AYKejVC4nJK9mwAsJi41/W+M
+-----END CERTIFICATE-----
+-----BEGIN CERTIFICATE-----
+MIIFFjCCAv6gAwIBAgIRAJErCErPDBinU/bWLiWnX1owDQYJKoZIhvcNAQELBQAw
+MldlTTKB3zhThV1+XWYp6rjd5JW1zbVWEkLNxE7GJThEUG3szgBVGP7pSWTUTsqX
+nLRbwHOoq7hHwg==
+-----END CERTIFICATE-----
+-----BEGIN CERTIFICATE-----
+MIIFYDCCBEigAwIBAgIQQAF3ITfU6UK47naqPGQKtzANBgkqhkiG9w0BAQsFADA/
+Dfvp7OOGAN6dEOM4+qR9sdjoSYKEBpsr6GtPAQw4dy753ec5
+-----END CERTIFICATE-----
+```
---
+++ main.go
@@ -0,0 +1,129 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"os"
+
+	corev1 "k8s.io/api/core/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+const (
+	secretAnnotKey = "cert-manager.io/secret-transform"
+	tlsPEMDataKey  = "tls.pem"
+)
+
+func init() {
+	log.SetLogger(zap.New())
+}
+
+func main() {
+	log := log.Log.WithName("entrypoint")
+
+	mgr, err := manager.New(config.GetConfigOrDie(), manager.Options{})
+	if err != nil {
+		log.Error(err, "unable to set up overall controller manager")
+		os.Exit(1)
+	}
+
+	rec := mgr.GetEventRecorderFor("secret-transformer")
+	c, err := controller.New("secret-transformer", mgr, controller.Options{
+		Reconciler: reconcile.Func(func(ctx context.Context, r reconcile.Request) (reconcile.Result, error) {
+			secret := corev1.Secret{}
+			err := mgr.GetClient().Get(ctx, r.NamespacedName, &secret)
+			switch {
+			case k8serrors.IsNotFound(err):
+				return reconcile.Result{}, nil
+			case err != nil:
+				return reconcile.Result{}, err
+			}
+
+			tlsKey, exists := secret.Data["tls.key"]
+			if !exists {
+				rec.Eventf(&secret, corev1.EventTypeWarning, "MissingTLSKey", "Secret %s does not contain a 'tls.key' data key", secret.Name)
+				return reconcile.Result{}, nil
+			}
+
+			tlsCrt, exists := secret.Data["tls.crt"]
+			if !exists {
+				rec.Eventf(&secret, corev1.EventTypeWarning, "MissingTLSCrt", "Secret %s does not contain a 'tls.crt' data key", secret.Name)
+				return reconcile.Result{}, nil
+			}
+
+			tlsPEMNew := []byte(fmt.Sprintf("%s%s", tlsKey, tlsCrt))
+
+			if tlsPEMOld, exists := secret.Data[tlsPEMDataKey]; exists && bytes.Compare(tlsPEMOld, tlsPEMNew) == 0 {
+				return reconcile.Result{}, nil
+			}
+
+			secret.Data[tlsPEMDataKey] = tlsPEMNew
+			err = mgr.GetClient().Update(ctx, &secret)
+			if err != nil {
+				return reconcile.Result{}, err
+			}
+
+			rec.Eventf(&secret, corev1.EventTypeNormal, "Transformed", "Added key %s to the Secret data", tlsPEMDataKey)
+			return reconcile.Result{}, nil
+		}),
+	})
+	if err != nil {
+		log.Error(err, "unable to set up individual controller")
+		os.Exit(1)
+	}
+
+	if err := c.Watch(&source.Kind{Type: &corev1.Secret{}}, handler.EnqueueRequestsFromMapFunc(func(o client.Object) []reconcile.Request {
+		if o.GetAnnotations() == nil {
+			return nil
+		}
+
+		var value string
+		var ok bool
+		if value, ok = o.GetAnnotations()[secretAnnotKey]; !ok {
+			return nil
+		}
+
+		switch value {
+		case tlsPEMDataKey:
+			return []reconcile.Request{{NamespacedName: types.NamespacedName{Namespace: o.GetNamespace(), Name: o.GetName()}}}
+		default:
+			rec.Eventf(o, corev1.EventTypeWarning, "InvalidSecretTransform", "Value %s is invalid for annotation %s", value, tlsPEMDataKey)
+			return nil
+		}
+	})); err != nil {
+		log.Error(err, "unable to watch Secrets")
+		os.Exit(1)
+	}
+
+	log.Info("starting manager")
+	if err := mgr.Start(signals.SetupSignalHandler()); err != nil {
+		log.Error(err, "unable to run manager")
+		os.Exit(1)
+	}
+}
